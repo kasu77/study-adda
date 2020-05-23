9925b14d5d5f0d5efefa765a5a46f8969a98b420
Quanta <quanta@aspirearch.localdomain>
2020-05-24 02:52:16 +0530

    Enable reasons for timer

--- study_bot/cogs/timer.py
+++ study_bot/cogs/timer.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Cog, command
 from discord.ext.commands import Context
-from discord import User, TextChannel
+from discord import User, TextChannel, Message
+import asyncio
 import re
 import asyncio
 
@@ -39,15 +40,25 @@ class Timer(Cog):
             await ctx.send('You want me to wait zero seconds lol? oK tHeN smh')
             return 0
 
-        await self.schedule(duration, ctx.author, ctx.channel)
+        await ctx.send('Send a reason for this timer within 10 seconds')
 
+        def check(message: Message):
+            return message.channel == ctx.channel and message.author == ctx.author
+
+        try:
+            reason_msg = await ctx.bot.wait_for('message', check=check, timeout=10)
+        except asyncio.TimeoutError:
+            await ctx.send(f"{ctx.author.mention} you took so long to type a reason. No timers for you.")
+            return
+
+        await self.schedule(duration, reason_msg.content, ctx.author, ctx.channel)
         await ctx.send(f"There ya go. I'll ping you in {hours}h {minutes}m {sec}s :>")
 
-    async def schedule(self, duration: int, owner: User, channel: TextChannel):
+    async def schedule(self, duration: int, reason, owner: User, channel: TextChannel):
         """Schedules a timer task for given duration"""
 
         async def timer_task():
             await asyncio.sleep(duration)
-            await channel.send(f"{owner.mention} Yo you there? Your timer is done, don't be dead!")
+            await channel.send(f"{owner.mention} Yo you there? Your timer called **{reason}** is done, don't be dead!")
 
         asyncio.create_task(timer_task())
